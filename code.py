d16c09536b3190a516bb9cf6d572589a1208d3aa
Shreya Ammanagi <74234689+ShreyaAmmanagi@users.noreply.github.com>
2022-02-10 15:30:47 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,21 +1,17 @@
 import time
-the = time.time() - 30*864000
+the = time.time() - (30 * 24 * 60 * 60)
 import os
 import shutil
+import stat
 path = input("enter the path of the folder you want Bin30 to get set to ")
+ctime = os.stat(path).st_mtime
 list_of_files = os.listdir(path)
 for file in list_of_files:
-    name,ext = os.path.splitext(file)
-    ext = ext[1:]
-    if os.path.exists(path):    
-        os.walk(path)
-        os.path.join(path)
-        ctime = os.stat(path).st_mtime
-        if ctime > the:
-            if ext == '':
-                shutil.rmtree()
-            else:
-                os.remove(path)
-    else:
-        print("not found!!!!!!!!")
+    #name,ext = os.path.splitext(file)
+    var = os.walk(path)
+    #ext = ext[1:]  
+    if ctime > the:
+        files = os.path.join(path,file)
+        os.chmod(files , stat.S_IWRITE)
+        os.remove(files)
 
